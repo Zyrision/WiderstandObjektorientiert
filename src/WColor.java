efb363e2463b1ca49c808cd76ebb298b0ffc11b8
Zyrision <111968938+Zyrision@users.noreply.github.com>
2022-08-25 12:10:27 +0200

    Add files via upload

--- src/WColor.java
+++ src/WColor.java
@@ -1,23 +1,23 @@
 import javax.crypto.KeyAgreement;
 
 public enum WColor {
-    BLACK(0,1,0),
-    BROWN(1,10,1),
-    RED(2,100,2),
-    ORANGE(3,1000,0),
-    YELLOW(4,10000,0),
-    GREEN(5,100000,0.5),
-    BLUE(6,1000000,0.25),
-    PURPLE(7,10000000,0.1),
-    GREY(8,100000000,0.05),
-    WHITE(9,1000000000,0),
-    GOLD(-1,10000000000.0,10),
-    SILVER(-2,100000000000.0,5);
-    private double multi;
-    private int colorValue;
-    private double toleranz;
+    BLACK(0,1.0,0.0),
+    BROWN(1,10.0,1.0),
+    RED(2,100.0,2.0),
+    ORANGE(3,1000.0,0.0),
+    YELLOW(4,10000.0,0.0),
+    GREEN(5,100000.0,0.5),
+    BLUE(6,1000000.0,0.25),
+    PURPLE(7,10000000.0,0.1),
+    GREY(8,100000000.0,0.05),
+    WHITE(9,1000000000.0,0.0),
+    GOLD(null,0.1,5.0),
+    SILVER(null,0.01,10.0);
+    private Double multi;
+    private Integer colorValue;
+    private Double toleranz;
 
-    private WColor(int colorValue, double multi, double toleranz){
+    private WColor(Integer colorValue, Double multi, Double toleranz){
         this.colorValue = colorValue;
         this.multi = multi;
         this.toleranz = toleranz;
